da2dc4ce8292188fd79bffd1630bd0c24c3a2a40
lmarchetti02 <00lucamarchetti00@gmail.com>
2024-01-02 13:47:03 +0100

    commented constants.hh

--- include/constants.hh
+++ include/constants.hh
@@ -8,7 +8,13 @@
 
 #define XY_PIXEL 22.5 * um
 #define XY_SUBPIXEL 7.5 * um
-#define Z_PIXEL 0.5 * mm 
+#define Z_PIXEL 0.5 * mm
 #define Z_WORLD 1 * mm
 
+/**
+ * Defines how the pixels should be irradiated:
+ *  - 0 for uniform irradiation of the central pixel
+ *  - 1 for uniform irradiation of the whole array
+ *  - 2 for pointwise irradiation of the center pf the central pixel
+ */
 #define BEAM_WIDTH 1
