f64fbb958fd5c924edef645d43ccb503156cfb3e
lmarchetti02 <00lucamarchetti00@gmail.com>
2024-01-29 18:38:38 +0100

    updated constants.hh

--- include/constants.hh
+++ include/constants.hh
@@ -3,19 +3,31 @@
 #include "G4SystemOfUnits.hh"
 #include "globals.hh"
 
+/**
+ * Whether to save the energy in the subpixels (before
+ * adding the charge sharing) in the .root file.
+ */
+constexpr G4bool INCLUDE_ORIGINAL = true;
+/**
+ * Whether to save the energy in the subpixels (after
+ * adding the charge sharing) in the .root file.
+ */
+constexpr G4bool INCLUDE_CS = true;
+
 constexpr G4int N_PIXEL = 15;
 constexpr G4int PIXEL_RATIO = 5;
 constexpr G4int N_SUBPIXEL = N_PIXEL * PIXEL_RATIO;
 
-constexpr G4double XY_PIXEL = 22.5 * um;
+constexpr G4double XY_PIXEL = 100 * um;
 constexpr G4double XY_SUBPIXEL = XY_PIXEL / PIXEL_RATIO;
 constexpr G4double Z_PIXEL = 0.5 * mm;
-
-constexpr const char *TGT_MATERIAL = "G4_CADMIUM_TELLURIDE";
 
 constexpr G4double TOLL_WORLD = 10 * um;
 constexpr G4double XY_WORLD = XY_SUBPIXEL * N_SUBPIXEL;
 constexpr G4double Z_WORLD = Z_PIXEL * 2;
+
+// The material of which the pixels are made.
+constexpr const char TGT_MATERIAL[] = "G4_CADMIUM_TELLURIDE";
 
 /**
  * Defines how the pixels should be irradiated:
@@ -26,11 +38,12 @@ constexpr G4double Z_WORLD = Z_PIXEL * 2;
 constexpr G4int BEAM_WIDTH = 1;
 
 /**
- * For charge sharing (see charge_sharing.hh):
- *  - `N_PARTS_ENERGY` is the number of parts in which each pixel energy
- *    is divided into
- *  - `SMEAR_WIDTH` id the standard deviation of the gaussian used for
- *    distributing the parts around
+ * The number of parts in which each pixel energy is divided into.
+ * See charge_sharing.hh for more info.
  */
 constexpr G4int N_PARTS_ENERGY = 50;
+/**
+ * The standard deviation of the gaussian used for distributing the energy bits around.
+ * See charge_sharing.hh for more info.
+ */
 constexpr G4double SMEAR_WIDTH = 10 * um;
