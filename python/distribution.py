cd58a91a90de2ce3b0b9fb7c678049d09fb5ccdb
lmarchetti02 <00lucamarchetti00@gmail.com>
2024-02-20 00:13:18 +0100

    updated python script

--- python/distribution.py
+++ python/distribution.py
@@ -1,11 +1,13 @@
+import sys
 import numpy as np
 from scipy import interpolate
 import matplotlib.pyplot as plt
 from numba import njit
+import pathlib
 
 
 @njit
-def get_mid_point(arr: np.ndarray):
+def _get_mid_point(arr: np.ndarray):
     res = np.empty(len(arr) - 1)
 
     for i in range(len(arr) - 1):
@@ -14,22 +16,35 @@ def get_mid_point(arr: np.ndarray):
     return res
 
 
-# distribution
-data = np.random.normal(0, 1, 10_000)
+def get_distribution(size: int):
+    size = int(size)
 
-# get cdf
-values, bins, _ = plt.hist(
-    data, bins="auto", density=True, cumulative=True, histtype="stepfilled"
-)
-bins = get_mid_point(bins)
+    # distribution
+    data = np.random.normal(0, 1, 10_000)
 
-# get inverse cdf
-inverse_cdf = interpolate.interp1d(values, bins)
+    # get cdf
+    values, bins, _ = plt.hist(
+        data, bins="auto", density=True, cumulative=True, histtype="stepfilled"
+    )
+    bins = _get_mid_point(bins)
 
-# get sample
-e = 1e-4
-u = np.random.uniform(e, 1 - e, 10_000)
-sample = inverse_cdf(u)
+    # get inverse cdf
+    inverse_cdf = interpolate.interp1d(values, bins)
 
-# save to file
-np.savetxt("spectrum.txt", sample)
+    # get sample
+    e = 1e-5
+    while True:
+        try:
+            u = np.random.uniform(e, 1 - e, size)
+            sample = inverse_cdf(u)
+            break
+        except ValueError as _:
+            continue
+
+    # save to file
+    np.savetxt(pathlib.Path("../build/spectrum.txt"), sample)
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    globals()[args[1]](*args[2:])
